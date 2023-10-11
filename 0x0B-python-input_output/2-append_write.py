a015aae527dc279d2a495e8821780ab33e0d33f6
derick mokua <derickmokua@outlook.com>
2023-10-11 02:47:52 +0000

    update

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-
+"""Module housing the append_write function."""
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
+    """Appends a string to a text file (UTF-8) and returns the number of characters added.
+
+Args:
+    filename (str, optional): The name of the file. Defaults to an empty string.
+    text (str, optional): The text to append to the file. Defaults to an empty string.
+
+Returns:
+    int: The number of characters appended to the file.
+"""
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        """Returns the count of characters appended to a file by this method."""
         return f.write(text)
