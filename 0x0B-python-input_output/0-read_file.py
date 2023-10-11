10da7e84d8596487f39f3d684124e1c87e9bf1f3
derick mokua <derickmokua@outlook.com>
2023-10-11 02:41:34 +0000

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-
+"""Module housing the read_file function."""
 
 def read_file(filename=""):
+	"""Reads a file and prints its contents to stdout.
+
+	Args:
+	filename (str, optional): The name of the file to read. Defaults to an empty string.
+	"""
+
     with open(filename, "r", encoding="UTF-8") as f:
         print(f.read(), end="")
