58595b1bb803a25f717c7cfb94ef1843fa64c2b2
derick mokua <derickmokua@outlook.com>
2024-06-18 11:08:49 -0700

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
+/**
+ * Define an object named 'myObject' with two properties:
+ * - 'type': A string indicating the type of the object.
+ * - 'value': A number representing the value of the object, initialized to 12.
+ */
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
+/**
+ * Add a method named 'incr' to 'myObject' that increments the 'value' property by 1.
+ */
 myObject.incr = function () {
   this.value++;
 };
