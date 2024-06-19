61abc708781611ba81484fdd434174638d0a6f9d
derick mokua <derickmokua@outlook.com>
2024-06-18 19:21:57 -0700

    advanced tasks bug fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/**
+ * Script that imports an array from a module and computes a new array.
+ * The new array contains the product of each element in the original array
+ * and its index.
+ */
 const list = require('./100-data.js').list;
 
 const newList = list.map((val, idx) => val * idx);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
+
 const dict = require('./101-data.js').dict;
 
 const newDict = {};
