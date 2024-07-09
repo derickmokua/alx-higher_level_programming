561ab401ca53fbe6584915600c41bfe9e3cd9949
derick mokua <derickmokua@outlook.com>
2024-07-09 13:24:52 -0700

    just a few lines

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script that retrieves the status from https://alx-intranet.hbtn.io/status"""
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
